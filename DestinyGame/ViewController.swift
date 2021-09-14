23ad241525e9a3db8c92aa7029882b55ec09bded
Giovanna Rodrigues <giovannamendessr@gmail.com>
2021-09-14 15:13:33 -0300

    Initial Commit

---
+++ DestinyGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DestinyGame
+//
+//  Created by Marcelo Rodrigues de Sousa on 14/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
